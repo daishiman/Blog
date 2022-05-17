b853ce57186f25706f6db90243dcefe2a36e8c7f
daishiman <daishimanju@gmail.com>
2022-05-17 20:07:06 +0900

    feat: ブログの調整

--- blog/components/Layout.js
+++ blog/components/Layout.js
@@ -1,21 +1,36 @@
 import Head from 'next/head';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
 const name = "Manju's Blog";
 export const siteTitle = 'manju Blog';
 
-export default function Layout({ children }) {
+export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/images/profile.png" className={utilStyles.borderCircle} />
-        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+        {home ? (
+          <>
+            <img src="/images/profile.png" className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`} />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <img src="/images/profile.png" className={`${utilStyles.borderCircle}`} />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
       </header>
       <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">←ホームへ戻る</Link>
+        </div>
+      )}
     </div>
   );
 }
